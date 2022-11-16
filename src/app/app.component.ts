cb5140082642c444d4e32dd65aaf125664fe31fa
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2022-11-16 14:24:12 +0530

    working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-reactive-formarray';
+  constructor(
+    public _service:AppService,
+
+  ){
+
+    this._service.getTodos();
+  }
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AppService {
-
-  constructor() { }
+  formGroup!: any;
+  constructor(private _fb: FormBuilder, private _http: HttpClient) {}
+  initForm() {
+    this.formGroup = this._fb.group({
+      array: this._fb.array([]),
+    });
+  }
+  push(item: any) {
+    const group = this._fb.group({
+      ...item,
+    });
+    this.array.push(group);
+  }
+  getTodos(): void {
+    this.initForm();
+    this._http.get('https://jsonplaceholder.typicode.com/todos').subscribe({
+      next: (todos: any) => {
+        todos.map((todo: any) => {
+          this.push(todo);
+        });
+      },
+    });
+  }
+  get array(): FormArray {
+    return this.formGroup.get('array') as FormArray;
+  }
 }
